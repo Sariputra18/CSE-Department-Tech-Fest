bf2f53103463775db5923ea35cc602b698f48d9e
ADITYA SAROHA <aditya@localhost.localdomain>
2025-02-10 07:22:34 +0530

    Removed a Para

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <ModelViewer />
-      <p style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px", fonFamily: "Poppins, sans- serif", FontSize: "18px" }}>Made with ❤️ by Aditya Saroha</p >
+      {/*<p style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px", fonFamily: "Poppins, sans- serif", FontSize: "18px" }}>Made with ❤️ by Aditya Saroha</p >*/}
     </>
 
   );
