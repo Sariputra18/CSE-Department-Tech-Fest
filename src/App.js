99b5d37ec04b79ac011cd03af1eb6b01babd3bde
ADITYA SAROHA <aditya@localhost.localdomain>
2025-02-11 06:49:40 +0530

    3x Enhanced the UI/UX of the landing page.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
       <BrowserRouter>
         <ModelViewer />
         <div className='logo'>Technotsav Logo</div>
-        {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with ❤️ by Technotsav Web Team</p >}
+        {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with <span style={{ fontSize: "1rem" }}>❤️</span> by Technotsav Web Team</p >}
         <Apply />
-        <Links facebook={"https://www.github.com/Aditya-138-12"} />
+        <Links facebook={"https://www.github.com/Aditya-138-12"} github={"https://github.com/CodeArena-SJCIT/CSE-Department-Tech-Fest"} />
       </BrowserRouter>
     </>
 
