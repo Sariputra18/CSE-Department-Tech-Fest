aebc70ea93227415fcb6ce438973ac4ff4fb0b12
ADITYA SAROHA <aditya@localhost.localdomain>
2025-02-11 07:54:27 +0530

    Added the About Component.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import ModelViewer from './components/ModelViewer';
 import './App.css';
 import Apply from './components/Apply/apply';
 import Links from './components/links/links';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/About/about';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <ModelViewer />
-        <div className='logo'>Technotsav Logo</div>
-        {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with <span style={{ fontSize: "1rem" }}>❤️</span> by Technotsav Web Team</p >}
-        <Apply />
-        <Links facebook={"https://www.github.com/Aditya-138-12"} github={"https://github.com/CodeArena-SJCIT/CSE-Department-Tech-Fest"} />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <ModelViewer />
+              <div className='logo'>Technotsav Logo</div>
+              {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with <span style={{ fontSize: "1rem" }}>❤️</span> by Technotsav Web Team</p >}
+              <Apply />
+              <Links facebook={"https://www.github.com/Aditya-138-12"} github={"https://github.com/CodeArena-SJCIT/CSE-Department-Tech-Fest"} />
+            </>
+          } />
+          <Route path="/about" element={
+            <>
+              <div className='logo'>Technotsav Logo</div>
+              {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with <span style={{ fontSize: "1rem" }}>❤️</span> by Technotsav Web Team</p >}
+              <Apply />
+              <About />
+              <Links facebook={"https://www.github.com/Aditya-138-12"} github={"https://github.com/CodeArena-SJCIT/CSE-Department-Tech-Fest"} />
+
+            </>
+          } />
+        </Routes>
       </BrowserRouter>
     </>
 
---
+++ src/components/About/about.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './about.css';
+
+const About = () => {
+    return (
+        <>
+            <div className="about-main">
+                <div className="about-container-photo"></div>
+                <div className="about-container">
+                    <h1>TECHNOTSAV 2K25</h1>
+                    <div className="about-container-m">
+                        <p className="p-about">Technotsav is the flagship annual technical festival of the Computer Science & Engineering (CSE) department, celebrating innovation, creativity, and technical excellence. It serves as a dynamic platform for students to showcase their skills, engage in hands-on learning experiences, and connect with like-minded tech enthusiasts.
+                            <br />
+                            What to Expect?
+                            <br />
+                            🔹 Hackathons & Coding Challenges – Test your problem-solving skills against the best minds.<br />
+                            🔹 Tech Talks & Workshops – Gain insights from industry experts and learn about cutting-edge technologies.<br />
+                            🔹 Project Exhibitions – Showcase your innovative projects and research.<br />
+                            🔹 Gaming & Robotics Competitions – Experience the thrill of AI, VR, and automation. <br />
+                            🔹 Startup & Idea Pitching – Bring your entrepreneurial ideas to life with expert mentorship. <br />
+                            🔹 Networking Events – Connect with like-minded individuals and build meaningful relationships. <br />
+                            🔹 Sponsorships – Get your company's logo on our website and gain exposure to a wider audience. <br />
+                            🔹 Awards Ceremonies – Celebrate your achievements and win exciting prizes. <br />
+                            🔹 Diversity & Inclusion Initiatives – Embrace diversity and create a welcoming environment for all.
+                            <br />
+                            <br />
+                            Whether you are a coder, designer, AI enthusiast, or just curious about tech, Technotsav has something for everyone. Join us in this celebration of technology and innovation!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default About;
